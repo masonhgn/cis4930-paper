ded844c5fa622c07a219f374f7de4741ef35ff6e
mason hagan <mason24h@gmail.com>
2023-12-02 20:50:34 -0500

    added new svm

--- main.py
+++ main.py
@@ -4,8 +4,63 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVR
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import cross_val_score
+
+# Load the data
+aal_df = pd.read_csv('moving_avgs.csv')
+
+# Assuming 'Date' column exists in your CSV
+aal_df['Date'] = pd.to_datetime(aal_df['Date'])
+
+# Split the data into features, labels, and dates
+X = aal_df[['20SMA', '5SMA']]
+y = aal_df['Close']  # Changed to series
+dates = aal_df['Date']
+
+# Split the dataset into training and testing sets, including dates
+X_train, X_test, y_train, y_test, dates_train, dates_test = train_test_split(X, y, dates, test_size=0.2, random_state=42)
+
+# Standardize the features
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+# Train the SVR model
+clf = SVR(kernel='linear')
+clf.fit(X_train, y_train)
+
+# Predict on the test set
+y_pred = clf.predict(X_test)
+
+# Calculate MSE and R-squared
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+print(f"Mean Squared Error: {mse}")
+print(f"R-squared: {r2}")
+
+# Cross-validation scores
+scores = cross_val_score(clf, X, y, cv=5)
+print("Cross-validated scores:", scores)
+print("Average cross-validation score:", scores.mean())
+
+# Plotting actual vs predicted prices
+plt.figure(figsize=(12, 6))
+plt.plot(dates_test, y_test, label='Actual Prices', color='blue', marker='o')
+plt.plot(dates_test, y_pred, label='Predicted Prices', color='red', marker='x')
+
+plt.title('Actual vs Predicted Prices Over Time')
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.legend()
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
 
 
+
+
+'''
 def main():
 	aal_df = pd.read_csv('AAL.csv')
 	wti_df = pd.read_csv('WTI.csv')
@@ -18,7 +73,7 @@ def main():
 	merged_df['Open_AAL'] = merged_df['Open_AAL'].shift(-1)
 	merged_df = merged_df.dropna()
 	#X = merged_df[['Lag5_AAL', 'Open_WTI', 'Lag1_WTI', 'Lag2_WTI', 'Lag3_WTI', 'Lag4_WTI', 'Lag5_WTI']]
-	X = merged_df[['Lag1_AAL', 'Lag2_AAL', 'Lag3_AAL', 'Lag4_AAL', 'Lag5_AAL', 'Open_WTI', 'Lag1_WTI', 'Lag2_WTI', 'Lag3_WTI', 'Lag4_WTI', 'Lag5_WTI']]
+	X = merged_df[['Lag1_AAL']]
 	y = merged_df['Open_AAL']
 	
 	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -77,3 +132,4 @@ def main():
 
 if __name__=="__main__":
 	main()
+'''
