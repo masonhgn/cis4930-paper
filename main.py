c12b510520336bc9305623ec14aa8d203ce57e9d
mason hagan <mason24h@gmail.com>
2023-12-05 10:54:44 -0500

    added svm model function

--- main.py
+++ main.py
@@ -6,130 +6,78 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import cross_val_score
-
-# Load the data
-aal_df = pd.read_csv('moving_avgs.csv')
-
-# Assuming 'Date' column exists in your CSV
-aal_df['Date'] = pd.to_datetime(aal_df['Date'])
-
-# Split the data into features, labels, and dates
-X = aal_df[['20SMA', '5SMA']]
-y = aal_df['Close']  # Changed to series
-dates = aal_df['Date']
-
-# Split the dataset into training and testing sets, including dates
-X_train, X_test, y_train, y_test, dates_train, dates_test = train_test_split(X, y, dates, test_size=0.2, random_state=42)
-
-# Standardize the features
-scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)
-
-# Train the SVR model
-clf = SVR(kernel='linear')
-clf.fit(X_train, y_train)
-
-# Predict on the test set
-y_pred = clf.predict(X_test)
-
-# Calculate MSE and R-squared
-mse = mean_squared_error(y_test, y_pred)
-r2 = r2_score(y_test, y_pred)
-print(f"Mean Squared Error: {mse}")
-print(f"R-squared: {r2}")
-
-# Cross-validation scores
-scores = cross_val_score(clf, X, y, cv=5)
-print("Cross-validated scores:", scores)
-print("Average cross-validation score:", scores.mean())
-
-# Plotting actual vs predicted prices
-plt.figure(figsize=(12, 6))
-plt.plot(dates_test, y_test, label='Actual Prices', color='blue', marker='o')
-plt.plot(dates_test, y_pred, label='Predicted Prices', color='red', marker='x')
-
-plt.title('Actual vs Predicted Prices Over Time')
-plt.xlabel('Date')
-plt.ylabel('Price')
-plt.legend()
-plt.xticks(rotation=45)
-plt.tight_layout()
-plt.show()
+import joblib
 
 
 
+def build_svm():
+	# Load the original data
+	aal_df = pd.read_csv('moving_avgs.csv')
 
-'''
-def main():
-	aal_df = pd.read_csv('AAL.csv')
-	wti_df = pd.read_csv('WTI.csv')
+	# Convert 'Date' to datetime
+	aal_df['Date'] = pd.to_datetime(aal_df['Date'])
 
-	aal_df = aal_df.rename(columns={'Open': 'Open_AAL','Lag1':'Lag1_AAL','Lag2':'Lag2_AAL','Lag3':'Lag3_AAL','Lag4':'Lag4_AAL','Lag5':'Lag5_AAL'})
-	wti_df = wti_df.rename(columns={'Open': 'Open_WTI','Lag1':'Lag1_WTI','Lag2':'Lag2_WTI','Lag3':'Lag3_WTI','Lag4':'Lag4_WTI','Lag5':'Lag5_WTI'})
+	# Split the data into features, labels, and dates
+	X = aal_df[['20SMA', '5SMA']]
+	y = aal_df['Close']
+	dates = aal_df['Date']
 
+	# Split the dataset into training and testing sets, including dates
+	X_train, X_test, y_train, y_test, dates_train, dates_test = train_test_split(X, y, dates, test_size=0.2, random_state=42)
 
-	merged_df = aal_df.merge(wti_df, on='Date', how='inner')
-	merged_df['Open_AAL'] = merged_df['Open_AAL'].shift(-1)
-	merged_df = merged_df.dropna()
-	#X = merged_df[['Lag5_AAL', 'Open_WTI', 'Lag1_WTI', 'Lag2_WTI', 'Lag3_WTI', 'Lag4_WTI', 'Lag5_WTI']]
-	X = merged_df[['Lag1_AAL']]
-	y = merged_df['Open_AAL']
-	
-	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+	# Standardize the features
 	scaler = StandardScaler()
-	X_train = scaler.fit_transform(X_train)
-	X_test = scaler.transform(X_test)
+	X_train_scaled = scaler.fit_transform(X_train)
+	X_test_scaled = scaler.transform(X_test)
 
-
+	# Train the SVR model
 	clf = SVR(kernel='linear')
+	clf.fit(X_train_scaled, y_train)
 
-	clf.fit(X_train, y_train)
+	joblib.dump(clf, 'svm_model.pkl')
 
-	y_pred = clf.predict(X_test)
+	# Save the scaler
+	joblib.dump(scaler, 'scaler.pkl')
 
-	from sklearn.metrics import mean_squared_error, r2_score
 
+
+
+	# Predict on the test set
+	y_pred = clf.predict(X_test_scaled)
+
+	# Calculate MSE and R-squared for the original test data
 	mse = mean_squared_error(y_test, y_pred)
 	r2 = r2_score(y_test, y_pred)
+	print(f"Original Data - Mean Squared Error: {mse}")
+	print(f"Original Data - R-squared: {r2}")
 
-	print(f"Mean Squared Error: {mse}")
-	print(f"R-squared: {r2}")
+	# Cross-validation scores on the original data
+	scores = cross_val_score(clf, X, y, cv=5)
+	print("Cross-validated scores:", scores)
+	print("Average cross-validation score:", scores.mean())
 
-	y_pred_all = clf.predict(X)
-
-	dates = pd.to_datetime(merged_df['Date'])
-
-	# Extract the actual and predicted opening prices
-	actual_prices = merged_df['Open_AAL']
-	predicted_prices = y_pred_all
-
-	dates = pd.to_datetime(merged_df['Date'])
-
-	# Define the number of x-axis ticks you want to display
-	num_ticks = 5  # Adjust this number as needed
-
-	# Create an array of indices to select ticks evenly spaced
-	tick_indices = np.linspace(0, len(dates) - 1, num_ticks, dtype=int)
-
-	# Get the corresponding dates for the selected tick indices
-	selected_dates = dates[tick_indices]
-
-	# Create an array of numbers for the X-axis (e.g., 0, 1, 2, ...)
-	x_values = np.arange(len(dates))
-
-	# Plot the actual and predicted prices over time with selected dates
-	plt.figure(figsize=(10, 6))
-	plt.plot(x_values, actual_prices, label='Actual Open Prices', color='b')
-	plt.plot(x_values, predicted_prices, label='Predicted Open Prices', color='r')
-	plt.xlabel('Date')  # Set the x-axis label to 'Date'
+	# Plotting actual vs predicted prices on the original data
+	plot_df = pd.DataFrame({'Date': dates_test, 'Actual': y_test, 'Predicted': y_pred})
+	plot_df.sort_values(by='Date', inplace=True)
+	plt.figure(figsize=(12, 6))
+	plt.plot(plot_df['Date'], plot_df['Actual'], label='Actual Prices', color='blue')
+	plt.plot(plot_df['Date'], plot_df['Predicted'], label='Predicted Prices', color='red')
+	plt.title('Actual vs Predicted Prices Over Time (Original Data)')
+	plt.xlabel('Date')
 	plt.ylabel('Price')
-	plt.title('Actual vs. Predicted Opening Prices Over Time (SVM Regression)')
-	plt.xticks(tick_indices, selected_dates, rotation=45)  # Set x-axis ticks as selected_dates with rotation
-	plt.legend(loc='upper left')
-	plt.grid(True)
+	plt.legend()
+	plt.xticks(rotation=45)
+	plt.tight_layout()
 	plt.show()
+
+
+
+
+
+
+
+def main():
+	build_svm()
 
 if __name__=="__main__":
 	main()
-'''
---
+++ trading.py
@@ -0,0 +1,69 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVR
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import cross_val_score
+import joblib
+
+
+def main():
+	clf = joblib.load('svm_model.pkl')
+	scaler = joblib.load('scaler.pkl')
+
+
+	
+	
+	df = pd.read_csv('new_data.csv')
+	df['Predicted_Close'] = df.apply(lambda row: predict(row['20SMA'], row['5SMA'],clf,scaler), axis=1)
+
+	# Initialize the first trade signal as NaN since there's no previous day to compare
+	df['Trade_Signal'] = float('nan')
+
+	# Generate trade signals (1 for Buy, 0 for Sell)
+	for i in range(1, len(df)):
+	    if df['Predicted_Close'].iloc[i] > df['Close'].iloc[i - 1]:
+	        df['Trade_Signal'].iloc[i] = 1  # Buy
+	    else:
+	        df['Trade_Signal'].iloc[i] = 0  # Sell
+
+	# The resulting DataFrame now has a column 'Trade_Signal' with buy/sell signals
+	print(df[['Date', 'Close', 'Predicted_Close', 'Trade_Signal']])
+
+		# Initialize trading variables
+	cash_balance = 50000  # Starting cash balance
+	stock_holding = 0     # Amount of stock held
+
+	# Trading simulation
+	for i in range(len(df)):
+	    if df['Trade_Signal'].iloc[i] == 1 and cash_balance > 0:  # Buy
+	        stock_holding = cash_balance / df['Close'].iloc[i]
+	        cash_balance = 0
+	    elif df['Trade_Signal'].iloc[i] == 0 and stock_holding > 0:  # Sell
+	        cash_balance = stock_holding * df['Close'].iloc[i]
+	        stock_holding = 0
+
+	# Calculate final portfolio value
+	final_portfolio_value = cash_balance + stock_holding * df['Close'].iloc[-1]
+
+	# Print results
+	print(f"Final Portfolio Value: ${final_portfolio_value:.2f}")
+
+
+def predict(sma20, sma5, clf, scaler):
+	new_data_for_today = pd.DataFrame({
+	    '20SMA': [sma20],
+	    '5SMA': [sma5]
+	})
+
+	#scale new data
+	new_scaled_data = scaler.transform(new_data_for_today)
+
+	#predict next day's price
+	next_day_prediction = clf.predict(new_scaled_data)
+	return next_day_prediction
+
+if __name__=="__main__":
+	main()
